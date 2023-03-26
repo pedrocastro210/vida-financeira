31a6e24523b2d226a27d562bc8263585eaae8255
Pedro Castro <phfdecastro@hotmail.com>
2023-03-26 16:55:31 -0300

    Update Project

--- src/components/RenderDashboard/index.jsx
+++ src/components/RenderDashboard/index.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RenderForm } from "../Form";
 import { TodoPage } from "../TodoList";
 import { v4 as uuidv4 } from "uuid";
 import Logo from "../../assets/logo.svg";
 import "./style.css";
 import { TodoPrice } from "../Price";
+import { DarkMode } from "../DarkMode";
 
 export function RenderDashboard({ setInPage }) {
   const myLocalStorage = localStorage.getItem("@nukenzie:value");
   const [todoList, setTodoList] = useState(
     myLocalStorage ? JSON.parse(myLocalStorage) : []
   );
+  const [mode, setMode] = useState("ligth");
 
   const addTodoToTodoList = (todo) => {
     const newTodo = { ...todo, id: uuidv4() };
@@ -26,11 +28,22 @@ export function RenderDashboard({ setInPage }) {
 
   todoList.map((todo) => soma.push(todo.valor));
 
+  useEffect(() => {
+    if (mode === "dark") {
+      document.querySelector(".main").classList.add("dark_mode");
+    } else {
+      document.querySelector(".main").classList.remove("dark_mode");
+    }
+  }, [mode]);
+
   return (
     <>
       <div className="header">
         <img src={Logo} alt="" />
-        <button onClick={() => setInPage(true)}>Início</button>
+        <div>
+          <DarkMode mode={mode} setMode={setMode} />
+          <button onClick={() => setInPage(true)}>Início</button>
+        </div>
       </div>
       <div className="main">
         <div>
